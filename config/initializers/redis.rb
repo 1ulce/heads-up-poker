eaf64163824ac5e0cf020694d0eb8e453f3f9629
1ulce <shun.1ulce@gmail.com>
2017-11-09 22:01:06 +0900

    [fix] redis setting

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,6 +1,8 @@
 require 'redis'
+Redis.current =  ""
 if ENV['RAILS_ENV'] == "development"
   Redis.current = Redis.new(:host => '127.0.0.1', :port => 6379)
 else
-  Redis.current = Redis.new(:host => 'redis://h:pc7bfd9a4468f39f94174fcdd6f6d4c191e12e332f91d71edf4a42176b9df848c@ec2-34-236-65-51.compute-1.amazonaws.com', :port => 63539)
+  Redis.current = Redis.new(:url => ENV['REDIS_URL'])
 end
+Redis.current
